03ebfaf918bb520d4e8d5a48d8f91d27b1b8d5a4
Joca0 <joaosalu22@outlook.com>
2024-03-26 15:21:50 -0300

    Modificado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inputs = new Scanner(System.in);
+        int NumeroSortido = new Random().nextInt(100);
+        System.out.println("Jogo de adivinhar um número de 0 a 100");
+
+        for (int i = 1; i < 6 ; i++) {
+            System.out.println("Digite um número de 0 a 100 Tentativa " + i );
+            int numeroEscolhido = inputs.nextInt();
+
+
+            if (numeroEscolhido < NumeroSortido) {
+                System.out.println("O número é maior que esse, tente novamente.");
+
+            } else if (numeroEscolhido > NumeroSortido) {
+                System.out.println("O número é menor que esse, tente novamente.");
+
+            } else {
+                System.out.println("Número correto parabéns!!");
+                break;
+            }
+        }
+        System.out.println("O número sortido era: " + NumeroSortido);
+
     }
+
+
+
+
 }
